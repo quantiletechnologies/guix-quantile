9ff10f621bb3fb04a42cfb4d3ece20456d216c7c
Phil.Beadling <phil.beadling@quantile.com>
2022-01-20 18:44:58 +0000

    initial hello

--- news.scm
+++ news.scm
@@ -1,2 +1,6 @@
 (channel-news
-  (version 0))
+ (version 0)
+ (entry
+  (commit "7e698df7200257be514089efdbe04d6af1d2535c")
+  (title (en "Welcome to Quantile Technologies' Guix Channel"))
+  (body (en "Packages not (yet) suitable for integrating into Guix proper, or other offshoots like Guix-Science will be hosted here for public consumption.  This initial commit comes with a package we hope to present at up-and-coming conferences which allows the creation of AWS Lambda Functions from Guix.  Use of the package will be covered in some future presentation, but those familiar with AWS Lambda can use it immediately."))))
